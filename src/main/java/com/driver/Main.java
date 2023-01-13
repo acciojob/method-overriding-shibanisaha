531f122bac4105da18535c5cee17ab074f5c85b7
shibanisaha <shibanis138@gmail.com>
2023-01-13 12:49:12 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
+    public static class A{
+        public static String meth(){
+           return "Invoking method from class A";
+        }
+    }
+   public static class B extends A{
+         public static String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B obj = new B();
+         obj.meth();
+    }
   
 }
